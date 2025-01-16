9f0a26ab5e1529bbe00d4815eb8edc2a78101ed4
Prasanna2911 <bpp1757@gmail.com>
2025-01-17 01:14:49 +0530

    hero

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,12 +1,14 @@
 import Button from "../components/Button";
 import { arrowRight } from "../assets/icons";
-import { statistics } from "../constants";
+import { shoes, statistics } from "../constants";
+import { bigShoe1 } from "../assets/images";
+import ShoeCard from "../components/ShoeCard";
 
 const Hero = () => {
   return (
     <section
       id="home"
-      className="w-full border-2 border-red-500 p-2 flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
+      className="w-full  p-2 flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
     >
       <div className="relative xl:w-2/5 flex flex-col justify-center items-start max-xl:padding-x pt-28">
         <p className="text-xl font-montserrat text-coral-red">
@@ -28,10 +30,32 @@ const Hero = () => {
         <div className="flex justify-start items-start flex-wrap w-full mt-20 border gap-16">
           {statistics.map((stat, index) => (
             <div key={stat.label}>
-              <p>{stat.value}</p>
-              <p>{stat.label}</p>
+              <p className="text-4xl font-palanquin font-bold">{stat.value}</p>
+              <p className="leading-7 font-montserrat text-slate-gray">
+                {stat.label}
+              </p>
             </div>
           ))}
+        </div>
+      </div>
+      <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
+        <img
+          src={bigShoe1}
+          alt="Shoe Collections"
+          width={610}
+          height={500}
+          className="relative z-10 object-contain"
+        />
+        <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
+          {shoes.map((shoe, index) => {
+            <div key={shoe}>
+              <ShoeCard
+                imgurl={shoe}
+                changeBigShoeImage={() => {}}
+                bigShoeImg=""
+              ></ShoeCard>
+            </div>;
+          })}
         </div>
       </div>
     </section>
